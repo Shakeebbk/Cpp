9039de6b076cf0b673090623d6b921d298ed593b
Shakeeb <shakeebbk@gmail.com>
2018-03-15 06:28:53 +0530

    sync xcode

---
+++ coding-ground-cpp/coding-ground-cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  coding-ground-cpp
+//
+//  Created by apple on 3/15/18.
+//  Copyright © 2018 apple. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
